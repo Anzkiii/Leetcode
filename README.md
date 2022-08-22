b158990956806b2b60fa737e814288a01186a0f7
Edward <edi.tzekov@gmail.com>
2022-08-22 22:47:28 +0300

    Sol1
    got it first try after a couple of hours :skull:

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Leetcode
  All of the leetcode problems ive done (adding more overtime)
+
+problem1.py
+https://leetcode.com/problems/roman-to-integer/
+
+
+
---
+++ problem1.py
@@ -0,0 +1,27 @@
+class Solution:
+    totalSum = 0
+    def romanToInt(self, s: str) -> int:
+        totalSum = 0
+        romanNums = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        skip = False
+        for i in range(len(s)):
+            print(s[i])
+            if not skip:
+                if s[i] == "I" or s[i] == "X" or s[i] == "C":
+                    try:
+                        if romanNums[s[i]] < romanNums[s[i+1]]:
+                            totalSum += romanNums[s[i+1]] - romanNums[s[i]]
+                            skip = True
+                        else:
+                            totalSum += romanNums[s[i]]
+                    except IndexError:
+                        totalSum += romanNums[s[i]]
+                else:
+                    totalSum += romanNums[s[i]]
+            else:
+                skip = False
+        return totalSum
+
+print(Solution().romanToInt("MMMMCXCIII"))
+
+
